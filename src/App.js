d69e69dd0a6205b75ecd58d1de60e4e2109a460a
Xavi <>
2018-06-23 16:15:37 +0200

    connection to graphcms

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 import logo from './logo.svg';
 import './App.css';
 
+const GET_BEDS = gql`
+{
+  allBeds {
+    id
+    reference
+    newPrice
+    price
+    header
+  }
+}
+`;
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Query query={GET_BEDS}>
+        {({loading, error, data}) => {
+          if (loading) return "Loading...";
+          if (error) return `Error! ${error.message}`;
+          return (
+            <div>
+            <h2>All Combinations</h2>
+            {data.allBeds.map(bed =>(
+              <p>Combination => {bed.id}</p>
+            ))}
+            </div>
+          );
+        }}
+        </Query>
+
       </div>
     );
   }
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GRAPHCMS_API = 'https://api.graphcms.com/simple/v1/cjfm499pw3ou801560kjiro3e'
+
+const client = new ApolloClient({
+  uri: GRAPHCMS_API
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 registerServiceWorker();
